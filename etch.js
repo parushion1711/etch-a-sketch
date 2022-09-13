5f54279f48c503def30ca48e5fabd3373367e2b2
Hayden <hayden.wong@pointerra.com>
2022-09-13 12:23:51 +0800

    "Added a button for resize of grid + external css"

--- etch.js
+++ etch.js
@@ -1,14 +1,26 @@
+let bigContain = document.querySelector('#main')
 let container = document.querySelector('#grid');
 let numColumns = 16;
 let numRows = 16;
 
-let btn1= document.createElement('button'); //adds a button for grid dimension selection 
-container.appendChild(btn1);
+//adds a button for grid dimension selection 
+let btn1= document.createElement('button'); 
+bigContain.appendChild(btn1);
 
-let sides = Number(prompt("Enter a number between 16 - 100", 16));
+//When button is clicked, new grid formed 
+btn1.addEventListener('click', () => {  
+    removed = bigContain.removeChild(container);
+    container = document.createElement("div");
+    bigContain.appendChild(container);
+    container.setAttribute('id', 'grid');
+    let sides = Number(prompt("Enter a number between 16 - 100", 16));
+    dimensOfGrid(sides); 
+    createGrid()
+    etchFunc();
+})
 
-
-function dimensOfGrid(num) { //checks if input number of grids is valid
+//checks if input number of grids is valid
+function dimensOfGrid(num) { 
     if (num < 16 || num > 100 || Number.isNaN(num)) {
         alert("Number is invalid, a default of 16x16 grid is created!");
         numColumns = 16;
@@ -19,36 +31,53 @@ function dimensOfGrid(num) { //checks if input number of grids is valid
     }
 }
 
-dimensOfGrid(sides)
+//checks if mouse is down
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
-for (row = 0; row < numRows; row++)  { //creates rows
-    let smlContainer = document.createElement("div");
-    if (row === 0) {
-        smlContainer.style.borderTop= "1px solid black";
-    }
-    smlContainer.style.display = 'flex';
-    smlContainer.style.height = `${1/numColumns*100}%`
-    smlContainer.style.width = '100%';
-    container.appendChild(smlContainer);
 
-    for (col = 0; col < numColumns; col++) { //creates grid
-        let cols = document.createElement("div");
-        if (col === 0) {
-            cols.setAttribute('style', 'border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black');
-        } else {
-        cols.setAttribute('style', 'border-bottom: 1px solid black; border-right: 1px solid black');
+//creates blank grid
+function createGrid() {
+    for (row = 0; row < numRows; row++)  { //creates rows
+        let smlContainer = document.createElement("div");
+        if (row === 0) {
+            smlContainer.style.borderTop= "1px solid black";
         }
-        cols.style.height = '100%';
-        cols.style.width = '100%';
-        cols.classList.add('unitGrid');
-        smlContainer.appendChild(cols);
+        smlContainer.style.display = 'flex';
+        smlContainer.style.height = `${1/numColumns*100}%`
+        smlContainer.style.width = '100%';
+        smlContainer.classList.add('rows')
+        container.appendChild(smlContainer);
+
+        for (col = 0; col < numColumns; col++) { //creates grid
+            let cols = document.createElement("div");
+            if (col === 0) {
+                cols.setAttribute('style', 'border-left: 1px solid black; border-bottom: 1px solid black; border-right: 1px solid black');
+            } else {
+            cols.setAttribute('style', 'border-bottom: 1px solid black; border-right: 1px solid black');
+            }
+            cols.style.height = '100%';
+            cols.style.width = '100%';
+            cols.classList.add('unitGrid');
+            smlContainer.appendChild(cols);
+        }
     }
 }
 
-let unitG = document.querySelectorAll('.unitGrid');
-let unitGArr = Array.from(unitG); //change nodeList to array
-unitGArr.forEach(grid => {
-    grid.addEventListener('mouseenter', (e) => { //turns black when hovered 
-       grid.style.backgroundColor = 'black' 
+//adds the coloring in for grid
+function etchFunc() {
+    let unitG = document.querySelectorAll('.unitGrid');
+    let unitGArr = Array.from(unitG); //change nodeList to array
+    unitGArr.forEach(grid => {
+        grid.addEventListener('mouseover', (e) => { //turns black when hovered and clicked
+            if (mouseDown) { 
+            grid.style.backgroundColor = 'black'
+            } 
+        });
     })
-});
+}
+
+//creates initial grid when page loads
+createGrid()
+etchFunc()
